929803a8575857afeedb852dc194bc042cc34f10
Yazin Castaño Moreno <yacamo04@hotmail.com>
2022-10-29 15:39:19 -0500

    Update App.js
    Se agrega manualmente los componentes en el app.js

--- r4soft/src/App.js
+++ r4soft/src/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 import Header from './components/Header';
 import { Footer } from './components/Footer';
+import Home from '/components/Home';
+import Metricas from '/components/Metricas';
 
 function App() {
   return (
 
     <div>
       <Header />
+      <Home />
+      <Metricas />
+      <section>
+      <Footer>
+      </section>
       <Footer />
     </div>
 
